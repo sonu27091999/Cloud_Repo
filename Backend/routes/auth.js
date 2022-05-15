80c3b9cf46cf150101ba03fb76cc0cec316c2850
papakagithub <shah.27091999@gmial.com>
2022-05-15 07:57:51 +0530

    demo commit

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -4,32 +4,13 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
 //Create a User using POST ""/api/auth/createuser. No login required
-router.post('/createuser', [
-    body('name', 'Enter a valid name').isLength({ min: 5 }),
-    body('email', 'Enter a valid email').isEmail(),
-    body('password', 'Password must be atleast 5 character').isLength({ min: 5 }),
-], async (req, res) => {
-    // If there are errors, return Bad request and the errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+router.get('/', (req,res)=>{
+    let obj={
+        name:"sonu",
+        age: 23
     }
-    // Checks whether the user with this email exists already
-    try {
-        let user = await User.findOne({ email: req.body.email });
-        if (user) {
-            return res.status(400).json({ error: "Sorry a user with this email already exists" });
-        }
-        user = await User.create({
-            name: req.body.name,
-            password: req.body.password,
-            email: req.body.email,
-        })
-        res.json(user);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Some error occured");
-    }
-})
+    res.json(obj);
+}
+)
 
 module.exports = router;
