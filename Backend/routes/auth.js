aa7644d502058959e9519f24697dd09f51ceaf4a
papakagithub <shah.27091999@gmial.com>
2022-05-18 09:42:27 +0530

    Creating login Endpoint & Sending auth token

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -43,8 +43,41 @@ router.post('/createuser', [
         res.json({authToken});
     } catch (error) {
         console.error(error.message);
-        res.status(500).send("Some error occured");
+        res.status(500).send("Internal Server error occured");
     }
 })
 
+
+//Authenticate a User using POST ""/api/auth/login. No login required
+router.post('/login', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password can not be blank').exists()
+], async (req, res) => {
+    // If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const {email,password}=req.body;
+    try {
+        let user = await User.findOne({ email});
+        if (!user) {
+            return res.status(400).json({ error: "please try with correct login crendentials." });
+        }
+        const passwordCompare=await bcrypt.compare(password,user.password);
+        if(!passwordCompare)
+            return res.status(400).json({ error: "please try with correct login crendentials." });
+        const data={
+            user:{
+                id:user.id
+            }
+        }
+        const authToken=jwt.sign(data,JWT_SECRET);
+        res.json({authToken});
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server error occured");
+    }
+})
 module.exports = router;
+
