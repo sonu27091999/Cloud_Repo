a91431103fe88be5df77b65c228659b259d0340e
papakagithub <shah.27091999@gmial.com>
2022-05-18 10:07:22 +0530

    Creating a middleware to decode user from a JWT

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const fetchuser = require('../middleware/fetchuser');
 
 const JWT_SECRET='Sonuisagood$boy'; 
 
-//Create a User using POST ""/api/auth/createuser. No login required
+//Route :1  Create a User using POST ""/api/auth/createuser. No login required
 router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 5 }),
     body('email', 'Enter a valid email').isEmail(),
@@ -48,7 +49,7 @@ router.post('/createuser', [
 })
 
 
-//Authenticate a User using POST ""/api/auth/login. No login required
+//Route : 2  Authenticate a User using POST ""/api/auth/login. No login required
 router.post('/login', [
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password can not be blank').exists()
@@ -79,5 +80,18 @@ router.post('/login', [
         res.status(500).send("Internal Server error occured");
     }
 })
+
+//Route :3  get loggedin user details using POST ""/api/auth/getuser. login required
+router.post('/getuser',fetchuser, async (req, res) => {
+    try {
+        let userId=req.user.id;
+        const user=await User.findById(userId).select("-password");
+        res.send(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server error occured");
+    }
+})
+
 module.exports = router;
 
