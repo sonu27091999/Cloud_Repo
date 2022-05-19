87194f8737d48fa26f8b730af7894d206c9eab78
papakagithub <shah.27091999@gmial.com>
2022-05-19 13:10:50 +0530

    iNotebook : Fetching Notes from Context

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,8 +2,29 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
+    const notesInitial=[
+        {
+          "_id": "628484158376728fd23bee19",
+          "user": "628471424db2a1a61e4217d4",
+          "title": "New title",
+          "description": "Please subscibe CWH",
+          "tag": "youtube",
+          "date": "2022-05-18T05:28:53.414Z",
+          "__v": 0
+        },
+        {
+          "_id": "62848fed4e445d5c89bb514e",
+          "user": "628471424db2a1a61e4217d4",
+          "title": "New title",
+          "description": "Please subscibe CWH",
+          "tag": "youtube",
+          "date": "2022-05-18T06:19:25.240Z",
+          "__v": 0
+        }
+      ];
+    const [notes, setNotes] = useState(notesInitial);
     return (
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes,setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
