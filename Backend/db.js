6f6abc3c2639d49b15a09c95b553934dc8318c44
sonu27091999 <96359340+sonu27091999@users.noreply.github.com>
2025-04-20 09:29:30 +0530

    Update

--- Backend/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose');
-const mongoURI='mongodb://localhost:27017/inotebook';
-
-const connectToMongo=()=>{
-    mongoose.connect(mongoURI,()=>{
-        console.log('Connected to MongoDB Successfully');
-    })
-}
-module.exports=connectToMongo;
